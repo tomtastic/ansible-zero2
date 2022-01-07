80ace28b9a12cce80f5dbd2bb6b8cd2578678d3e
Tom Matthews <trcm@pm.me>
2022-01-07 10:41:44 +0000

    gradual improvements here, working config at least on MacOS

--- files/gadget.sh
+++ files/gadget.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bash
 # Configure USB gadgets (run as root from /etc/rc.local)
 
-### Create a sparse file for the mass storage gadget
-MASS_STORAGE0="/home/pi/mass_storage.lun0"
-if [[ ! -f "$MASS_STORAGE0" ]]; then
-	# 8GB seems plenty?
-    dd if=/dev/zero of="$MASS_STORAGE0" bs=1M seek=8192 count=0
-    #echo <<EOFCAT | fdisk -u "$MASS_STORAGE0"
-    #EOFCAT
-    losetup -o 4096 /dev/loop0 "$MASS_STORAGE0"
-    mkdosfs -v /dev/loop0 -S 4096 -g 64/8 -n ZERO2USB
-    losetup -d /dev/loop0
-fi
-
 ### Configuring USB gadgets via ConfigFS
 
 #### Define our composite gadget parameters
 GADGET_NAME="zero2"
 MANUFACTURER="Raspberry"
 PRODUCT="PiZero2 Composite Gadget"
-SERIAL=$(awk -F": " '/^Serial/ {print $2}' /proc/cpuinfo)
 HOST="48:6f:73:74:50:43" # "HostPC"
 SELF0="42:61:64:55:53:42" # "USB0"
 SELF1="42:61:64:55:53:43" # "USB1"
+MASS_STORAGE0="/home/pi/mass_storage.lun0"
 SCSI_INQUIRY="Zero2 Mass Storage"
 
 # Dont touch these...
@@ -30,12 +18,13 @@ OS=$(cat /tmp/os.fingerprint)
 CONFIGFS_HOME="/sys/kernel/config"
 GADGET_DIR="$CONFIGFS_HOME"/usb_gadget/"$GADGET_NAME"
 GADGET_LANG="0x409" # English language strings
-ID_VENDOR="0x04b3" # IBM ?
-ID_PRODUCT="0x4123" # Nonsense
 BCD_USB="0x0200" # USB2
-BCD_DEVICE="0x0100" # v1.0.0
+BCD_DEVICE="0x3066" # ?
 B_DEVICECLASS="0x02"
 B_DEVICESUBCLASS="0x00"
+ID_VENDOR="0x1d6b" # ?
+ID_PRODUCT="0x0104" # ?
+SERIAL=$(awk -F": " '/^Serial/ {print $2}' /proc/cpuinfo)
 MAX_POWER=250
 
 #### Create a composite gadget
@@ -55,6 +44,7 @@ echo "$PRODUCT" > strings/"$GADGET_LANG"/product
 #### --> Ethernet gadget
 # If we're not connected to MacOS, configure an RNDIS-type adapter
 if [[ "$OS" != "MacOS" ]]; then
+    #### CONFIG 1 ####
     mkdir -p configs/c.1/strings/"$GADGET_LANG"
     echo "CDC ACM+MassStorage+RNDIS" > configs/c.1/strings/"$GADGET_LANG"/configuration
     echo "$MAX_POWER" > configs/c.1/MaxPower
@@ -68,20 +58,35 @@ if [[ "$OS" != "MacOS" ]]; then
     echo "MSFT100" > os_desc/qw_sign # Microsoft
     echo "1" > os_desc/use
 fi
-# We'll always also configure an ECM-type adapter
+
+#### CONFIG 2 ####
 mkdir -p configs/c.2/strings/"$GADGET_LANG"
-echo "CDC ACM+MassStorage+ECM" > configs/c.2/strings/"$GADGET_LANG"/configuration
+
+#### --> Ethernet gadget
+# We'll always also configure an NCM-type adapter
+echo "CDC ACM+MassStorage+NCM" > configs/c.2/strings/"$GADGET_LANG"/configuration
 echo "$MAX_POWER" > configs/c.2/MaxPower
-mkdir -p functions/ecm.usb0
-echo "$HOST" > functions/ecm.usb0/host_addr
-echo "$SELF1" > functions/ecm.usb0/dev_addr
+mkdir -p functions/ncm.usb0
+echo "$HOST" > functions/ncm.usb0/host_addr
+echo "$SELF1" > functions/ncm.usb0/dev_addr
 
 #### --> Serial gadget
 mkdir -p functions/acm.usb0
 
 #### --> Mass Storage gadget
+### Create a sparse file for the mass storage gadget
+if [[ ! -f "$MASS_STORAGE0" ]]; then
+    # 8GB seems plenty?
+    dd if=/dev/zero of="$MASS_STORAGE0" bs=1M seek=8192 count=0
+    #echo <<EOFCAT | fdisk -u "$MASS_STORAGE0"
+    #EOFCAT
+    losetup -o 4096 /dev/loop0 "$MASS_STORAGE0"
+    mkdosfs -v /dev/loop0 -S 4096 -g 64/8 -n ZERO2USB
+    losetup -d /dev/loop0
+fi
+
 mkdir -p functions/mass_storage.usb0
-echo 0 > functions/mass_storage.usb0/stall
+echo 1 > functions/mass_storage.usb0/stall
 echo 0 > functions/mass_storage.usb0/lun.0/cdrom
 echo 0 > functions/mass_storage.usb0/lun.0/ro
 echo 0 > functions/mass_storage.usb0/lun.0/nofua
@@ -93,11 +98,16 @@ echo "$MASS_STORAGE0" > functions/mass_storage.usb0/lun.0/file
 if [[ "$OS" != "MacOS" ]]; then
     ln -s configs/c.1 os_desc
     ln -s functions/rndis.usb0 configs/c.1
+else
+    ln -s configs/c.2 os_desc
 fi
-ln -s functions/ecm.usb0 configs/c.2
+ln -s functions/ncm.usb0 configs/c.2
 ln -s functions/acm.usb0 configs/c.2
 ln -s functions/mass_storage.usb0 configs/c.2
 
-udevadm settle -t 5 || true
-echo "" > UDC
+#udevadm settle -t 5 || true
 basename "$(find /sys/class/udc -type l)" > UDC
+
+# Enable Serial
+systemctl enable getty@ttyGS0.service
+systemctl start getty@ttyGS0.service
